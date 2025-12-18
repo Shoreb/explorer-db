3780f06e02a79a56f0674424b8491689a995c8d4
Shoreb Pava <shorebpava18@gmail.com>
2025-12-18 10:01:41 -0500

    Frontend basico con HTML, CSS Y JS para listar tablas

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 from app.routers import health, db_test, metadata
 
 app = FastAPI(title="Explorador de Bases de Datos")
@@ -6,3 +8,11 @@ app = FastAPI(title="Explorador de Bases de Datos")
 app.include_router(health.router)
 app.include_router(db_test.router)
 app.include_router(metadata.router)
+
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+def serve_frontend():
+    with open("frontend/templates/index.html", "r", encoding="utf-8") as f:
+        return f.read()
