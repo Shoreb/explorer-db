b8e02c472fb8b4024c616998881acb5789b906e2
Shoreb Pava <shorebpava18@gmail.com>
2025-12-19 13:49:59 -0500

    Permitir hacer clic en las tablas para ver las columnas dinámicamente

--- app/static/app.js
+++ app/static/app.js
@@ -1,3 +1,4 @@
+let connectionData = null;
 const form = document.getElementById("connection-form");
 const resultDiv = document.getElementById("result");
 
@@ -12,6 +13,8 @@ form.addEventListener("submit", async (event) => {
         password: document.getElementById("password").value,
         database: document.getElementById("database").value
     };
+
+    connectionData = data;
 
     try {
         const response = await fetch(
@@ -49,11 +52,55 @@ function showTables(tables) {
     const ul = document.createElement("ul");
 
     tables.forEach(table => {
+        const tableName = Object.values(table)[0];
         const li = document.createElement("li");
-        li.textContent = Object.values(table)[0];
+
+        li.textContent = tableName;
+        li.style.cursor = "pointer";
+
+        li.addEventListener("click", () => {
+            loadColumns(tableName);
+        });
+
         ul.appendChild(li);
     });
 
     resultDiv.appendChild(ul);
 }
 
+//Funcion para cargar columnas
+async function loadColumns(tableName) {
+    resultDiv.innerHTML += `<h4>Columnas de ${tableName}</h4>`;
+
+    try {
+        const response = await fetch(
+            `/metadata/dynamic/tables/${tableName}/columns`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(connectionData)
+            }
+        );
+
+        const result = await response.json();
+
+        if (!response.ok) {
+            throw new Error(result.detail || "Error al cargar columnas");
+        }
+
+        const ul = document.createElement("ul");
+
+        result.columns.forEach(col => {
+            const li = document.createElement("li");
+            li.textContent = `${col.COLUMN_NAME} (${col.DATA_TYPE})`;
+            ul.appendChild(li);
+        });
+
+        resultDiv.appendChild(ul);
+
+    } catch (error) {
+        resultDiv.innerHTML += `<p style="color:red">${error.message}</p>`;
+    }
+}
